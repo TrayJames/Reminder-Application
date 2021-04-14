acdf6086bc4f1eb75139ef518caebc15c0490adb
Khoa Nguyen <kanguyen18@earlham.edu>
2021-04-14 16:26:24 -0400

    Initial Commit

---
+++ Reminder App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder App
+//
+//  Created by KHOA NGUYEN on 4/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
