b94b01467c6eedf50222de77c7916c1138367c70
Khoa Nguyen <kanguyen18@earlham.edu>
2021-04-17 19:57:08 -0400

    Signup and Login - Khoa

--- Reminder App/AppDelegate.swift
+++ Reminder App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "wQgOQdNcPleod2NBkNXMdT0t3apFdMprtUN88ySW" // <- UPDATE
+                $0.clientKey = "p7tQlr01NFTOkvN9NVQWwKKU8iEHQoTCFAjYqKxE" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
@@ -30,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
---
+++ Reminder App/ReminderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderViewController.swift
+//  Reminder App
+//
+//  Created by KHOA NGUYEN on 4/15/21.
+//
+
+import UIKit
+
+class ReminderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
