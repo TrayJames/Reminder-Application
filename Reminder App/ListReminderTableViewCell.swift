9970c0e38e39a41a4101c6b5301bd1b98d24e25f
TrayJames <65936907+TrayJames@users.noreply.github.com>
2021-04-27 23:01:59 -0400

    Updates

---
+++ Reminder App/ListReminderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListReminderTableViewCell.swift
+//  Reminder App
+//
+//  Created by Tra-Vaughn James on 4/27/21.
+//
+
+import UIKit
+
+class ListReminderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
